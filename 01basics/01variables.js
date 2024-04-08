17f9bf0f71579912a79b4074a485ebb1f09c4c7a
AnkitKashyap01 <114510776+AnkitKashyap01@users.noreply.github.com>
2024-04-08 20:05:47 +0000

    container change done

---
+++ 01basics/01variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345;
+let accountEmail = "xyz@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";//Not a good practice
+let accountState;//will give not defined
+
+//accountId = 2 //can't change the value of const keyword as it is not allowed
+console.log(accountId)
+
+accountEmail = 'abc@gmail.com'
+accountPassword='212121'
+accountCity='Bengaluru'
+
+//console.table is used to print more than one variable
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+//var is not used becoz it gives issues in block scope and functional scope. 
