31203d937cebd6d523dd89f544eb47df898806ca
AnkitKashyap01 <114510776+AnkitKashyap01@users.noreply.github.com>
2024-04-23 17:31:12 +0000

    datatypes and conversion

--- 01basics/02dataTypes.js
+++ 01basics/02dataTypes.js
@@ -1,3 +1,8 @@
+//Data Types    -->
+
+
+
+
 /*Javascript --> older version(8-10 years ago) does't support class, modules, and arrows 
   so we "use strict" to treat all JS code as newer version.*/
   
@@ -13,6 +18,7 @@ let age = 21  //number type
 let isloggedIn = false //boolean type
 
 /*Primitive data types -->
+
 1. number --> range upto 2 to power 53.
 2. bigint --> when range of number exceed 2 to power 53.
 3. string --> written in " ".
@@ -27,9 +33,78 @@ let area
 //object --> will study in later
 
 //Typeof --> tells the type of variable.
-console.log(typeof 21)
-console.log(typeof "ankit")
-console.log(typeof isloggedIn)
-console.log(typeof state)
-console.log(typeof area)
 
+// console.log(typeof 21)
+// console.log(typeof "ankit")
+// console.log(typeof isloggedIn)
+// console.log(typeof state)
+// console.log(typeof area)
+
+
+
+
+//Datatype Conversion Confusion --->
+
+
+
+
+// let score = 33
+// console.log(typeof(score)); --> will give number.
+
+let score="33"
+console.log(typeof(score)); //--> will give a string value.
+
+let valueInNumber = Number(score) //--> Number() is used to convert a variable in any type into number type. 
+console.log(typeof valueInNumber);  //--> will give a number value.
+
+let score1="33abc";
+let valueInNumber1 = Number(score1);
+console.log(typeof valueInNumber1);//--> will give number.
+console.log(valueInNumber1);//--> will give NaN (Not a Number).
+
+
+let score2=null
+console.log(typeof(score2)); //will give an object.
+
+let valueInNumber2 = Number(score2) 
+console.log(typeof valueInNumber2); //will give a number.
+console.log(valueInNumber2); // will give 0.
+
+
+
+let score3=undefined
+console.log(typeof(score3)); //--> will give a undefined value.
+
+let valueInNumber3 = Number(score3) //--> Number() is used to convert a variable in any type into number type. 
+console.log(typeof valueInNumber3);// will give a number value.
+console.log(valueInNumber3);//will give NaN.
+
+
+let score4=true
+console.log(typeof(score4)); //--> will give a boolean value.
+
+let valueInNumber4 = Number(score4) //--> Number() is used to convert a variable in any type into number type. 
+console.log(typeof valueInNumber4);//will give a Number.
+console.log(valueInNumber4);
+
+/*Note---> "33"->33
+            "33abc"->NaN
+            true->1, false->0.*/
+            
+let islogged = 1;
+let booleanIsLogged = Boolean(islogged);
+console.log(booleanIsLogged);//true
+
+
+let islogged1 = "";
+let booleanIsLogged1 = Boolean(islogged1);
+console.log(booleanIsLogged1);//false
+
+let islogged2 = "ankit";
+let booleanIsLogged2 = Boolean(islogged2);
+console.log(booleanIsLogged2);//true
+
+let someNumber = 33
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
